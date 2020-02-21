b54d399c9d22ef7361a5fe404f1ead9aeb894bc7
Aashir Aamir Khan <hello@justaashir.com>
2020-02-21 08:24:13 +0500

    First Initial Commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="layout">
+    <div class="has-background-light">
+      <div class="container">
+        <nav class="navbar is-light is-fixed" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="#">
+              <img src="../../static/Startup..png" alt />
+            </a>
+
+            <a role="button" class="navbar-burger burger" data-target="navbar">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbar" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item">Home</a>
+
+              <a class="navbar-item">Documentation</a>
+
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">More</a>
+
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">About</a>
+                  <a class="navbar-item">Jobs</a>
+                  <a class="navbar-item">Contact</a>
+                  <hr class="navbar-divider" />
+                  <a class="navbar-item">Report an issue</a>
+                </div>
+              </div>
+              <div class="navbar-item">
+                <div class="buttons">
+                  <a class="button is-primary">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a class="button is-outlined is-primary">Log in</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+.layout {
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+    <div class="columns">
+      <div class="column is-12 has-text-centered">
+        <section class="section has-background-light" style="padding-top:50px;">
+          <h3 class="title is-1">
+            Completely Free & Open Source Template
+            <br />for Startups
+          </h3>
+          <p class="subtitle is-5" style="max-width:60rem;margin:0 auto;">
+            The Modern Look & Feel with Seamless Technology 'Gridsome', A Free Responsive Startup Template made with
+            <a href="https://www.bulma.io">
+              Bulma
+            </a>
+            You can easily modify the template with your company's primary color, and all the illustrations are from @undraw.
+          </p>
+          <div class="section">
+            <div class="buttons is-centered">
+              <a class="button is-primary is-large">Get Started -></a>
+              <a class="button is-primary is-outlined is-large">Contact Us</a>
+            </div>
+          </div>
+          <img src="../../static/cover-hero.svg" alt width="700" style="margin-top:-50px;" />
+        </section>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Startup Starter - Gridsome + Bulma | Justaashir"
+  }
+};
+</script>
+
+<style>
+</style>
