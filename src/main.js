275cda012eb8275da436cd13dee5ae6fe458da5c
Aashir Aamir Khan <hello@justaashir.com>
2020-02-21 09:35:47 +0500

    Added Features Section

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import DefaultLayout from '~/layouts/Default.vue'
 
 import '~/assets/scss/main.scss'
 
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <Layout>
-    <div class="columns">
+    <div class="columns is-multiline">
+      <!-- Landing Page -->
       <div class="column is-12 has-text-centered">
         <section class="section has-background-light" style="padding-top:50px;">
           <h3 class="title is-1">
@@ -22,15 +23,63 @@
           </div>
           <img src="../../static/cover-hero.svg" alt width="700" style="margin-top:-50px;" />
         </section>
+        <div style="height: 250px; overflow: hidden;margin-top:-200px;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-88.37,127.40 C150.00,150.00 349.20,-49.99 603.49,106.67 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
       </div>
+
+      <!-- Features -->
+      <div class="column is-12">
+        <section class="has-text-centered" style="padding-bottom:60px;">
+          <span class="tag is-light is-primary is-large">FEATURES</span>
+          <h3 class="title is-2" style="margin-top:30px;">Some of the Features</h3>
+          <p style="max-width:60rem;margin:0 auto;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique dicta vitae est quas possimus quibusdam ea commodi? Voluptates atque hic blanditiis, vitae fugiat saepe magni ea laudantium sunt, velit voluptas?</p>
+        </section>
+      </div>
+      <div class="column">
+        <div class="columns" style="padding-bottom:50px;">
+      <div class="column has-text-centered">
+        <ApertureIcon size="5x"/>
+        <h3 class="is-size-4 has-text-dark has-text-weight-bold">Sample Text</h3>
+        <p class="has-text-grey" style="padding:0px 5px;">Some Sample Text, put anything here</p>
+        <div class="has-text-primary has-text-bold is-size-5" style="margin-top:10px;">Learn More -></div>
+      </div>
+      <div class="column has-text-centered">
+        <ApertureIcon size="5x"/>
+        <h3 class="is-size-4 has-text-dark has-text-weight-bold">Sample Text</h3>
+        <p class="has-text-grey" style="padding:0px 5px;">Some Sample Text, put anything here</p>
+        <div class="has-text-primary has-text-bold is-size-5" style="margin-top:10px;">Learn More -></div>
+      </div>
+      <div class="column has-text-centered">
+        <ApertureIcon size="5x"/>
+        <h3 class="is-size-4 has-text-dark has-text-weight-bold">Sample Text</h3>
+        <p class="has-text-grey" style="padding:0px 5px;">Some Sample Text, put anything here</p>
+        <div class="has-text-primary has-text-bold is-size-5" style="margin-top:10px;">Learn More -></div>
+      </div>
+      <div class="column has-text-centered">
+        <ApertureIcon size="5x"/>
+        <h3 class="is-size-4 has-text-dark has-text-weight-bold">Sample Text</h3>
+        <p class="has-text-grey" style="padding:0px 5px;">Some Sample Text, put anything here</p>
+        <div class="has-text-primary has-text-bold is-size-5" style="margin-top:10px;">Learn More -></div>
+      </div>
+    </div>
+    </div>
     </div>
   </Layout>
 </template>
 
 <script>
+import { ApertureIcon } from 'vue-feather-icons'
+
 export default {
   metaInfo: {
     title: "Startup Starter - Gridsome + Bulma | Justaashir"
+  },
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    ApertureIcon
   }
 };
 </script>
